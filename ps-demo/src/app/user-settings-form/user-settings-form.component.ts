f5d40885c2700094808ed6fa12cbf86abb79d342
Christian Layman <christianlayman@hotmail.com>
2022-05-16 21:26:39 -0500

    userData model creation

--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserData } from '../data/user-data';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+    
+    originalUserData : UserData = {
+      name: null,
+      emailOffers: null,
+      interfaceStyle: null,
+      subscriptionType: null, 
+      notes: null
+  };
+
+    userData : UserData = { ...this.originalUserData };
 
   constructor() { }
 
